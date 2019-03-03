3d9da7ce1f9e22c59f30dba90b5b2b08b93b2b91
mostafa-elrosasy <mostafa12345688@yahoo.com>
2019-03-03 22:34:34 +0200

    "handled errors"

--- ProfileSystem/urls.py
+++ ProfileSystem/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import url
 from ProfileSystem import views
+from django.urls import path
 
 
 urlpatterns = [
-
-  
+    path('', views.profile.as_view()),
 
 ]
--- ProfileSystem/views.py
+++ ProfileSystem/views.py
@@ -23,6 +23,7 @@ def log_errors(ex,id):
 # The function is used to get the ID of the user who sent the token
 # by decoding the Jwt. Parameter: the request, Returns: user ID
 def get_user_ID(request):
+
     try:
         auth = get_authorization_header(request).split()
         token=auth[1]
@@ -30,78 +31,70 @@ def get_user_ID(request):
         payload= jwt.decode(token,secret)
         return payload['user_id']
     except Exception as ex:
-        log_errors(str(ex),1)
-        return Response("An error has happened!", status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        return -1
 
 
-class View_Profile(APIView):
+
+
+class profile(APIView):
 
 # This function returns a JSON that contains the profile data of the user
 # that sent the request, it accepts GET requests
     def get(self, request):
         try:
             id =get_user_ID(request)
+            if (id == -1):
+                return Response("The jwt token isn't correct", status=status.HTTP_401_UNAUTHORIZED)
             # checks if the user doesn't have an account
-            if( not (Profile.objects.filter(user=id).exists())):
+            if(not(Profile.objects.filter(user=id).exists())):
                 return Response("This user doesn't have a profile yet", status=status.HTTP_400_BAD_REQUEST)
             profiles= Profile.objects.filter(user=id)
             serializer= ProfileSerializer(profiles,many=True)
             # Stores "View Profile" activity in the database
             log(user=User.objects.filter(id=id).first(), action="Viewed Profile",)
             return Response(serializer.data)
-        except TypeError as ex:
-            log_errors(str(ex),1)
-            return Response("The jwt token isn't correct", status=status.HTTP_500_INTERNAL_SERVER_ERROR)
         except Exception as ex:
             log_errors(str(ex),id)
-            return Response("An error has happened!", status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            return Response("Please contact the fan page!", status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 # This function is used to create new profiles for registered users, it accepts POST requests
     def post(self, request):
         try:
-            json_data=request.data
             if(request.data["national_front"] =="" or request.data["national_back"] == ""):
                 return Response("The national ID images can't be  blank", status=status.HTTP_400_BAD_REQUEST)
             # gets the ID of the user that sent the request
             id =get_user_ID(request)
-            if(not(User.objects.filter(id=id).exists)):
-                log_errors(str(ex),1)
-                return Response("An error has happened!", status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            if (id == -1):
+                return Response("The jwt token isn't correct", status=status.HTTP_401_UNAUTHORIZED)
             # checks if this user already has a profile
             if(Profile.objects.filter(user=id).exists()):
                 return Response("This user already has a profile", status=status.HTTP_400_BAD_REQUEST)
-            json_data['user']=id
+            request.data['user']=id
             serializer = ProfileSerializer(data=request.data)
             if serializer.is_valid():
                 serializer.save()
                 # Stores "Create Profile" activity in the database
                 log(user=User.objects.filter(id=id).first(), action="Created Profile",)
                 return Response(serializer.data, status=status.HTTP_201_CREATED)
-            log(user=User.objects.filter(id=id).first(),action="Tried to Create Profile",)
-            
+            log(user=User.objects.filter(id=id).first(),action="Tried to Create Profile",)           
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        except TypeError as ex:
-            log_errors(str(ex),1)
-            return Response("The jwt token isn't correct", status=status.HTTP_500_INTERNAL_SERVER_ERROR)
         except Exception as ex:
             log_errors(str(ex),id)
-            return Response("An error has happened!", status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            return Response("Please contact the fan page!", status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 # This class is used to edit the user's profile data. When accessed whith GET request it
 # returns the profile data of the user who made the request. When accessed with POST request
 # it gets the new data from the user to edit his profile
     def put(self, request):
         try:
-            json_data=request.data
             if(request.data["national_front"]=="" or request.data["national_back"] == ""):
                 return Response("The national ID images can't be  blank", status=status.HTTP_400_BAD_REQUEST)
             # gets the ID of the user that sent the request
             id =get_user_ID(request)
-            if(not(User.objects.filter(id=id).exists)):
-                log_errors(str(ex),1)
-                return Response("An error has happened!", status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            if (id == -1):
+                return Response("The jwt token isn't correct", status=status.HTTP_401_UNAUTHORIZED)
             # checks if the user doesn't have an account
-            if( not (Profile.objects.filter(user=id).exists())):
+            if(not (Profile.objects.filter(user=id).exists())):
                 return Response("This user doesn't have a profile yet", status=status.HTTP_400_BAD_REQUEST)
             profiles=Profile.objects.filter(user=id).first()
             serializer = ProfileSerializer(profiles,data=request.data)
@@ -112,10 +105,7 @@ class View_Profile(APIView):
                 return Response(serializer.data, status=status.HTTP_201_CREATED)
             log(user=User.objects.filter(id=id).first(),action="Tried to edit Profile",)
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        except TypeError as ex:
-            log_errors(str(ex),1)
-            return Response("The jwt token isn't correct", status=status.HTTP_500_INTERNAL_SERVER_ERROR)
         except Exception as ex:
             log_errors(str(ex),id)
-            return Response("An error has happened!", status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            return Response("Please contact the fan page!", status=status.HTTP_500_INTERNAL_SERVER_ERROR)
     
