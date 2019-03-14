894411f38e481b2a456187be3df51a12667d457c
Ahmed Ebeid <a.ebeid@hotmail.com>
2019-03-14 23:48:38 +0200

    images url

--- RT_Website_19/urls.py
+++ RT_Website_19/urls.py
@@ -20,9 +20,12 @@ from rest_framework_jwt.views import verify_jwt_token
 from AuthenticationSystem import urls as ASystem
 from ProfileSystem import urls as ProfileSystem
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('api/auth/', include(ASystem)),
     path('token-verify/', verify_jwt_token),
     path('admin/', admin.site.urls),
     path('api/profile/', include(ProfileSystem)),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
